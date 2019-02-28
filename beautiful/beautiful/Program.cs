73162137a3a24abaa00d14e330384f1ee743a055
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-02-28 14:00:08 +1300

    first commit

---
+++ beautiful/beautiful/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beautiful
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To change the color of the text .
+            Console.ForegroundColor = ConsoleColor.White;
+
+                
+            Console.WriteLine("Please enter your first name");
+            string  Fname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name");
+                string  Lname = Console.ReadLine();
+
+            /*string concatenation*/
+            Console.WriteLine(Fname + " " + Lname);
+
+            //replacement method.
+            Console.WriteLine("{0} {1}", Fname, Lname);
+
+            //Dollar sign method.
+            Console.WriteLine($"{Fname} {Lname}");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Write your address here");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Write your email address here");
+            string Emailaddress = Console.ReadLine();
+
+            Console.WriteLine("Write your age here");
+            int age = int.Parse(Console.ReadLine());
+
+            //giving a heading.
+            Console.WriteLine("**********Personal information * *****");
+            Console.WriteLine($"{username}\n{address}\n{Emailaddress}\n{age}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ newdesign/newdesign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newdesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,rajveer here");
+
+            Console.WriteLine("feeling bit better now");
+            Console.Write("hope everything will get well soon");
+            Console.Write("be positive");
+            Console.ReadLine();
+        }
+    }
+}
